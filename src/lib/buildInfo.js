9f5cbbe5889aab0457c85bbdb302b8d8a74e09c5
mskian <10300271+mskian@users.noreply.github.com>
2025-03-26 22:22:47 +0530

    tweaks and fixes

--- src/lib/buildInfo.js
+++ src/lib/buildInfo.js
@@ -1,13 +1,7 @@
-import fs from "fs";
-import path from "path";
 import { DateTime } from "luxon";
-
-const packageJsonPath = path.resolve("package.json");
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
-const astroVersion = packageJson.dependencies.astro || "Unknown";
 
 const lastBuildTime = DateTime.now()
   .setZone("Asia/Kolkata")
   .toFormat("EEEE, d MMMM yyyy, hh:mm:ss a");
 
-export { astroVersion, lastBuildTime };
+export { lastBuildTime };
